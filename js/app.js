94c0f6283180b19e4436bca8e78ee46cd37946de
Francieli Viane <francielifrv@gmail.com>
2016-06-19 15:57:49 -0300

    Francieli/Lucas/Marianne/Thomaz  - Renderiza dados do banco de dados na tabela e remove array estático

--- js/app.js
+++ js/app.js
@@ -1,48 +1,39 @@
 $(function(){
-	var data = {
-    users: [
-            {
-              "_id": "5747c24c5d0bbba60619ed56",
-              "gender": "Male",
-              "email": "zevinagre@gmail.com",
-              "name": "Zé Mauro"
-            },
-            {
-              "_id": "574c7cc21201c90300b7252f",
-              "gender": "Female",
-              "email": "mariab@bethania.com",
-              "name": "Maria Bethânia"
-            },
-            {
-              "_id": "5754a95ef42fc50300b8f408",
-              "gender": "Male",
-              "email": "marcelom@gmail.com",
-              "name": "Marcelo Martelo"
-            },
-            {
-              "_id": "5754d72af42fc50300b8f409",
-              "gender": "Female",
-              "email": "janaina@gmail.com",
-              "name": "Janaina Jana"
-            },
-            {
-              "_id": "575604f634501d2c0b245572",
-              "gender": "Male",
-              "email": "drmilton@gmail.com",
-              "name": "Milton Nascimento"
-            }
-          ]
-        };
-
 	var Crud = function() {
 
 	}, crud;
 
 	Crud.prototype.renderList = function () {
 		var template = Handlebars.compile(document.getElementById('users-template').innerHTML);
-		var content = document.getElementById('content');
-		content.innerHTML = template(data);
+
+		var render = function(data) {
+			var user_list = {
+				users:data
+			};
+
+			var content = document.getElementById('content');
+			content.innerHTML = template(user_list);
+		}
+
+		var error = function(data) {
+            console.dir(data);
+    };
+
+		this.makeAjaxRequest("GET", render, error)
 	};
+
+	Crud.prototype.makeAjaxRequest = function(method, render, error, params) {
+        $.ajax({
+            url: "http://localhost:8181/api/users" + (params? ("/" + params): ""),
+            method: method,
+            success: function(data) {
+                render(data);
+            },
+            error: function(data) {
+                error(data);
+            }
+        });
+    }
 
 	Crud.prototype.init = function () {
 		this.renderList();
